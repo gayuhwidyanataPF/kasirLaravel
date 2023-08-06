385c13380056c535f022bbcabfe9988cf74f4966
gayuhwidyanataPF <termosdingin2@gmail.com>
2023-08-06 14:46:24 +0700

    perubahan

--- resources/views/dashboard/layouts/nav.blade.php
+++ resources/views/dashboard/layouts/nav.blade.php
@@ -3,11 +3,7 @@
     <!-- Sidebar Toggle-->
     <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
     <!-- Navbar Brand-->
-<<<<<<< HEAD
     <a class="navbar-brand ps-3" href="/">Dashboard</a>
-=======
-    <a class="navbar-brand ps-3" href="/dashboard">Dashboard</a>
->>>>>>> b537cadb8236cf67f9921f58c289e71736bd61eb
 \    <!-- Navbar-->
     <ul class="navbar-nav d-none d-md-inline-block ms-auto me-0 me-md-3 my-2 my-md-0">
         <li class="nav-item dropdown">
